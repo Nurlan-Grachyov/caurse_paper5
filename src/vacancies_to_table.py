8a8d7c5598e1100246911333f500868f1851db74
Nurlan <nurlan.grachyov@mail.ru>
2024-11-25 00:36:28 +0400

    начало

---
+++ src/vacancies_to_table.py
@@ -0,0 +1,57 @@
+# import psycopg2
+#
+# from src.get_vacancies import get_vacancies
+#
+#
+# def vacancies_to_table(vacancies):
+#     conn_params = {
+#         "host": "localhost",
+#         "database": "vacancies",
+#         "user": "postgres",
+#         "password": "07052001",
+#     }
+#
+#     with psycopg2.connect(**conn_params) as conn:
+#         with conn.cursor() as cur:
+#             try:
+#                 cur.execute("DROP TABLE IF EXISTS vacancies;")
+#                 cur.execute(
+#                     "CREATE TABLE vacancies (name_vacancy text not null, location varchar(100) not null, salary_from INT not null, salary_to INT not null, currency varchar(10) not null, url text not null);"
+#                 )
+#                 for vacancy in vacancies:
+#                     name_vacancy = vacancy["name"]
+#                     location = vacancy["area"]["name"]
+#                     salary_from = (
+#                         vacancy["salary"]["from"]
+#                         if vacancy["salary"]["from"] is not None
+#                         else 0
+#                     )
+#                     salary_to = (
+#                         vacancy["salary"]["to"]
+#                         if vacancy["salary"]["to"] is not None
+#                         else 0
+#                     )
+#                     currency = vacancy["salary"]["currency"]
+#                     url_vacancy = vacancy["alternate_url"]
+#                     cur.execute(
+#                         "INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s)",
+#                         (
+#                             name_vacancy,
+#                             location,
+#                             salary_from,
+#                             salary_to,
+#                             currency,
+#                             url_vacancy,
+#                         ),
+#                     )
+#                     conn.commit()
+#                     cur.execute("SELECT * FROM vacancies;")
+#             except Exception as e:
+#                 print(e)
+#                 print("Ошибка в get_vacancies")
+#                 return []
+#         return "Вакансии добавлены в таблицу"
+#
+#
+# if __name__ == "__main__":
+#     print(vacancies_to_table(get_vacancies()))
