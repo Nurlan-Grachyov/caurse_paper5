9da537f9f13e2edc07900b999c783911c68401ff
Nurlan <nurlan.grachyov@mail.ru>
2024-12-02 23:29:46 +0400

    вопросы по типизации

--- src/vacancies_to_table.py
+++ src/vacancies_to_table.py
@@ -44,59 +44,59 @@ def vacancies_to_table(vacancies: List[dict]) -> str:
     """Функция, добавляющая вакансии в таблицу"""
     load_dotenv()
     password = os.getenv("DATABASE_PASSWORD")
-    conn_params = psycopg2.connect(
-        host="localhost",
-        database="vacancies",
-        user="postgres",
-        password=password,
-    )
+    conn_params = {
+        "host":"localhost",
+        "database":"vacancies",
+        "user":"postgres",
+        "password":password,
+    }
 
-    # with psycopg2.connect(**conn_params) as conn:
-    #     with conn.cursor() as cur:
-    cur = conn_params.cursor()
-    try:
-        cur.execute("DROP TABLE IF EXISTS vacancies;")
-        cur.execute(
-            "CREATE TABLE vacancies (name_vacancy text not null, "
-            "employer varchar(100) not null, "
-            "location varchar(100) not null, "
-            "salary_from INT not null, salary_to INT not null, "
-            "currency varchar(10) not null, url text not null,"
-            "FOREIGN KEY (employer) REFERENCES employers(employer));"
-        )
-        for vacancy in vacancies:
-            name_vacancy = vacancy["name"]
-            employer = vacancy["employer"].get("name")
-            location = vacancy["area"]["name"]
-            salary_from = (
-                vacancy["salary"]["from"]
-                if vacancy["salary"]["from"] is not None
-                else 0
-            )
-            salary_to = (
-                vacancy["salary"]["to"] if vacancy["salary"]["to"] is not None else 0
-            )
-            currency = vacancy["salary"]["currency"]
-            url_vacancy = vacancy["alternate_url"]
-            cur.execute(
-                "INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                (
-                    name_vacancy,
-                    employer,
-                    location,
-                    salary_from,
-                    salary_to,
-                    currency,
-                    url_vacancy,
-                ),
-            )
-            conn_params.commit()
-            # cur.close()
-            # conn_params.close()
-        return "Вакансии добавлены в таблицу"
-    except Exception as e:
-        print(e)
-        return "Ошибка в get_vacancies"
+    with psycopg2.connect(**conn_params) as conn:
+        with conn.cursor() as cur:
+    # cur = conn_params.cursor()
+            try:
+                cur.execute("DROP TABLE IF EXISTS vacancies;")
+                cur.execute(
+                    "CREATE TABLE vacancies (name_vacancy text not null, "
+                    "employer varchar(100) not null, "
+                    "location varchar(100) not null, "
+                    "salary_from INT not null, salary_to INT not null, "
+                    "currency varchar(10) not null, url text not null,"
+                    "FOREIGN KEY (employer) REFERENCES employers(employer));"
+                )
+                for vacancy in vacancies:
+                    name_vacancy = vacancy["name"]
+                    employer = vacancy["employer"].get("name")
+                    location = vacancy["area"]["name"]
+                    salary_from = (
+                        vacancy["salary"]["from"]
+                        if vacancy["salary"]["from"] is not None
+                        else 0
+                    )
+                    salary_to = (
+                        vacancy["salary"]["to"] if vacancy["salary"]["to"] is not None else 0
+                    )
+                    currency = vacancy["salary"]["currency"]
+                    url_vacancy = vacancy["alternate_url"]
+                    cur.execute(
+                        "INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                        (
+                            name_vacancy,
+                            employer,
+                            location,
+                            salary_from,
+                            salary_to,
+                            currency,
+                            url_vacancy,
+                        ),
+                    )
+                    conn.commit()
+                    # cur.close()
+                    # conn_params.close()
+                return "Вакансии добавлены в таблицу"
+            except Exception as e:
+                print(e)
+                return "Ошибка в get_vacancies"
 
 
 if __name__ == "__main__":
